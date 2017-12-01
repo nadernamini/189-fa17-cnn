fixed error calculation in nearestneighbors

nn_classifier.py
@@ -18,13 +18,13 @@ class NN:
 
         for i in range(train_data.shape[0]):
             X = np.vstack((X, train_data[i, 2].flatten()))
-        self.train_data = {'X': X, 'y': train_data[:, 1].reshape((train_data[:, 1].shape[0], 1))}
+        self.train_data = {'X': X, 'y': train_data[:, 1]}
 
         X = np.empty((0, len(val_data[0, 2].flatten())))
         for i in range(val_data.shape[0]):
             X = np.vstack((X, val_data[i, 2].flatten()))
 
-        self.val_data = {'X': X, 'y': val_data[:, 1].reshape((val_data[:, 1].shape[0], 1))}
+        self.val_data = {'X': X, 'y': val_data[:, 1]}
 
         print(val_data.shape, train_data.shape)
 
@@ -46,7 +46,17 @@ class NN:
         over randomly selected data points. To save computation.
         """
         chc = np.random.randint(self.val_data['X'].shape[0], size=self.sample_size)
-        return self.model.score(self.val_data['X'][chc], self.val_data['y'][chc])
+        X, y = self.val_data['X'][chc], self.val_data['y'][chc]
+        yhat = self.model.predict(X)
+        # self.model.score(self.val_data['X'][chc], self.val_data['y'][chc])
+
+        return self.calc_err(y, yhat)
+
+    def calc_err(self, y, yhat):
+        err = 0
+        for i in range(self.sample_size):
+            err += np.linalg.norm(y[i] - yhat[i]) ** 2
+        return err / self.sample_size
 
     def get_train_error(self):
         """
@@ -54,4 +64,8 @@ class NN:
         over randomly selected data points. To save computation.
         """
         chc = np.random.randint(self.train_data['X'].shape[0], size=self.sample_size)
-        return self.model.score(self.train_data['X'][chc], self.train_data['y'][chc])
+        X, y = self.train_data['X'][chc], self.train_data['y'][chc]
+        yhat = self.model.predict(X)
+        # self.model.score(self.train_data['X'][chc], self.train_data['y'][chc])
+
+        return self.calc_err(y, yhat)